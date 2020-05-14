26637f1bf4b484cd4f5a879e16f4bf8609435a1d
Konstantinos Kampadais <csd3954@csd.uoc.gr>
2020-05-14 16:07:28 +0300

    failara to e

--- Phase4/Compiler/targetCode.c
+++ Phase4/Compiler/targetCode.c
@@ -6,6 +6,33 @@
 extern double QuadNo;
 
 
+void generate_ADD (struct quad *quad){}
+void generate_SUB (struct quad *quad){}
+void generate_MUL (struct quad *quad){}
+void generate_DIV (struct quad *quad){}
+void generate_MOD (struct quad *quad){}
+void generate_NEWTABLE (struct quad *quad){}
+void generate_TABLEGETELM (struct quad *quad){}
+void generate_TABLESETELEM(struct quad *quad){}
+void generate_ASSIGN (struct quad *quad){}
+void generate_NOP (struct quad *quad){}
+void generate_JUMP(struct quad *quad){}
+void generate_IF_EQ(struct quad *quad){}
+void generate_IF_NOTEQ(struct quad *quad){}
+void generate_IF_GREATER(struct quad *quad){}
+void generate_IF_GREATEREQ(struct quad *quad){}
+void generate_IF_LESS(struct quad *quad){}
+void generate_IF_LESSEQ(struct quad *quad){}
+void generate_NOT(struct quad *quad){}
+void generate_OR(struct quad *quad){}
+void generate_PARAM(struct quad *quad){}
+void generate_CALL(struct quad *quad){}
+void generate_GETRETVAL(struct quad *quad){}
+void generate_FUNCSTART(struct quad *quad){}
+void generate_RETURN(struct quad *quad){}
+void generate_FUNCEND(struct quad *quad){}
+
+
 typedef void (*generator_func_t) (struct quad*);
 
 generator_func_t generators[] = {
@@ -32,14 +59,16 @@ generator_func_t generators[] = {
 	generate_TABLESETELEM,
 	generate_GETRETVAL,
 	generate_NOP,
-	generate_RETURN
+	generate_RETURN,
 
 };
 
 
 void generate(void){
 
-
+	for (size_t i = 0; i < QuadNo; i++) {
+		/* code */
+	}
 
 
 }
